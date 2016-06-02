bb7100c3b1e9ebcf383adb0a4736ddb5e8f0f6c3
Nancy Majors <nmajors@gmail.com>
2016-06-02 16:32:03 -0500

    keeping files

--- app/modules/portfolio/controller.js
+++ app/modules/portfolio/controller.js
@@ -9,7 +9,7 @@ class PortfolioController {
 
         name: "Weather Watcher",
         description: "My final project for The Iron Yard. A fully responsive web app where each user can save addresses for their friends and family and keep track of weather conditions and severe weather situations for all of their saved locations in one place.",
-        screenshot: './assets/images/weather-watcher-full.png',
+        screenshot: './assets/images/weather-watcher.jpg',
         site: "https://nmajors.github.io/final-project/",
         code: "https://github.com/nmajors/final-project.git",
         tech: "HTML, CSS/SASS, AngularJS, Firebase, Angular Google Maps, OpenWeatherMap API, and Bootstrap for form styles.",
@@ -19,8 +19,8 @@ class PortfolioController {
         name: "Powell Peralta Skateboards",
         description: "A fully responsive site layout created with @media queries.",
         screenshot: './assets/images/powell-peralta-full.png',
-        site: "https://nmajors.github.io/powell-peralta-master/",
-        code: "https://github.com/nmajors/powell-peralta-master/",
+        site: "https://nmajors.github.io/08-powell-peralta-master/",
+        code: "https://github.com/nmajors/08-powell-peralta-master/",
         tech: "HTML and CSS/SASS.",
         sliding: false
       },
@@ -36,7 +36,7 @@ class PortfolioController {
       {
         name: "To Do List",
         description: "My first Firebase project, a simple to do list app where the user can create, read, update, and delete todos.",
-        screenshot: './assets/images/to-do-list.jpg',
+        screenshot: './assets/images/to-do-list.png',
         site: "https://nmajors.github.io/28-firebase/dist/",
         code: "https://github.com/nmajors/28-firebase.git",
         tech: "HTML, CSS/SASS, AngularJS, and Firebase.",
@@ -45,7 +45,7 @@ class PortfolioController {
       {
         name: "Weather API",
         description: "This was a vanilla JavaScript assignment on using Navigator.geolocation and GET to receive current weather and the seven day forecast for the user's current location from the OpenWeatherMap API. Almost all of the HTML elements and attributes in this project were created with JavaScript.",
-        screenshot: './assets/images/weather-api.jpg',
+        screenshot: './assets/images/weather-api.png',
         site: "https://nmajors.github.io/18-weather-api/dist/",
         code: "https://github.com/nmajors/18-weather-api.git",
         sliding: false,
